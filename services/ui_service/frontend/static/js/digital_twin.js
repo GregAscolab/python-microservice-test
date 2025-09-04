7d5b2694c9cf56bec1dfbf1f4a6c2395d9930ce1
Grégory Janiszewski <g.janiszewski@ascolab.fr>
2025-09-04 12:04:32 +0200

    Add back the 3D view of the digital_twin parts

--- services/ui_service/frontend/static/js/digital_twin.js
+++ services/ui_service/frontend/static/js/digital_twin.js
@@ -58,17 +58,48 @@ function initDigitalTwinsPage() {
             const traces = [];
             // This part of the logic remains similar, but it reads from `modelState`
             for (const partName in modelState) {
-                if (!modelState[partName] || !modelState[partName]['points']) continue;
+                if (!modelState[partName]) continue;
+                
+                if (modelState[partName]['plan']) {
+                    const plan = modelState[partName]['plan'];
+                    if (plan.length > 0) {
+                        // console.log("plan=", plan)
+                        const xp = plan.map(p => p[0]);
+                        const yp = plan.map(p => p[1]);
+                        const zp = plan.map(p => p[2]);
 
-                const part = modelState[partName]['points'];
-                const x = part.map(p => p[0]);
-                const y = part.map(p => p[1]);
-                const z = part.map(p => p[2]);
+                        const p = {
+                            x: xp,
+                            y: yp,
+                            z: zp,
+                            // Utiliser les indices des sommets pour définir les triangles de la surface.
+                            // Le plan est un quadrilatère, donc nous avons besoin de deux triangles.
+                            // triangle 1: points 0, 1, 2
+                            // triangle 2: points 0, 2, 3
+                            // Etc...
+                            i: [0, 0, 0, 0, 3, 3, 4, 4],
+                            j: [1, 2, 1, 5, 2, 6, 5, 6],
+                            k: [2, 3, 5, 4, 6, 7, 6, 7],
+                            type: 'mesh3d',
+                            opacity: 0.8,
+                            showscale: false,
+                            name: partName + "Plan"
+                        };
+                        traces.push(p);
+                    }
+                }
 
-                traces.push({
-                    x: x, y: y, z: z,
-                    mode: 'lines+markers', type: 'scatter3d', name: partName
-                });
+                if (modelState[partName]['points']) {
+                    const part = modelState[partName]['points'];
+                    const x = part.map(p => p[0]);
+                    const y = part.map(p => p[1]);
+                    const z = part.map(p => p[2]);
+
+                    traces.push({
+                        x: x, y: y, z: z,
+                        mode: 'lines+markers', type: 'scatter3d', name: partName
+                    });
+                }
             }
             traces.push(boundingPoints[0]);
             Plotly.react(digitalTwinContainer, traces, layout);
